a8437501e9d7179100f9da324fee40a7987859ca
Sairam Koripelly <skkoripe@amazon.com>
2025-05-15 08:04:35 +0530

    Init

---
+++ src/main/java/com/bentham/voiceagent/BenthamVoiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.bentham.voiceagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenthamVoiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenthamVoiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bentham/voiceagent/BenthamVoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentham.voiceagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenthamVoiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
